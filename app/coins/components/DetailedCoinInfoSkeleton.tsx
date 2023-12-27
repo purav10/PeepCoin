16cde688ed58bba17bc57496f10ff1f5ff39c0ef
purav10 <ce210004009@iiti.ac.in>
2023-12-27 15:40:13 +0530

    adjusted coindetails skeleton

--- app/coins/components/DetailedCoinInfoSkeleton.tsx
+++ app/coins/components/DetailedCoinInfoSkeleton.tsx
@@ -1,20 +1,20 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
 const DetailedCoinInfoSkeleton = () => (
-  <>
-  <div className="flex flex-col gap-4">
-    <Skeleton className="h-24 w-24 rounded-full" /> 
-    <Skeleton className="h-6 w-[375px]" /> 
+  <div className="flex-grow">
+    <div className="flex flex-row gap-4">
+      <Skeleton className="h-16 w-28 rounded-full" />
+      <div className="flex flex-col flex-grow gap-4">
+        <Skeleton className="h-6 w-full" /> 
+        <Skeleton className="h-6 w-full" />
+      </div>
+    </div>
+
+    <div className="flex flex-row gap-4 mt-4">
+      <Skeleton className="h-10 w-1/2" /> 
+      <Skeleton className="h-10 w-1/2" />
+    </div>
   </div>
-    <div className="flex flex-col gap-4">
-    <Skeleton className="h-4 w-full" /> 
-    <Skeleton className="h-full rounded-md w-[400px]" /> 
-    </div>
-    <div className="flex flex-col gap-4">
-    <Skeleton className="h-full rounded-half w-[450px]" /> 
-    </div>
-</>
 );
 
 export default DetailedCoinInfoSkeleton;
-
