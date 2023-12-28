8157e78b3e33676a499344af2692fdd307a8e24d
purav10 <ce210004009@iiti.ac.in>
2023-12-28 21:36:07 +0530

    changed NewsAPI to theguardianAPI

--- app/coins/components/NewsFeed.tsx
+++ app/coins/components/NewsFeed.tsx
@@ -1,20 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { Card, CardDescription, CardHeader, CardContent } from '@/components/ui/card';
+import styled from 'styled-components';
 
 interface Article {
-  title: string;
-  description: string;
-  url: string;
-  urlToImage: string;
+  webTitle: string;
+  webUrl: string;
+  webPublicationDate: string;
 }
 
 interface Props {
   coinId: string;
 }
 
+const API_KEY = "873b8c2b-0b7f-4527-a088-89cf8fb341d8";
+
+const NewsGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 20px;
+  @media (max-width: 600px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
 const NewsFeed: React.FC<Props> = ({ coinId }) => {
   const [articles, setArticles] = useState<Article[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
@@ -25,14 +36,11 @@ const NewsFeed: React.FC<Props> = ({ coinId }) => {
       setError(null);
 
       try {
-        const response = await fetch(`https://newsapi.org/v2/everything?q=${coinId}&apiKey=6cf1ae7a05e54341819fded828c94dcc`);
-        if (!response.ok) throw new Error('Failed to fetch news articles. Trust me this works on my computer🥲');
+        const response = await fetch(`https://content.guardianapis.com/search?q=${coinId}&api-key=${API_KEY}`);
+        if (!response.ok) throw new Error('Failed to fetch news articles');
         const data = await response.json();
-        if (data.articles.length === 0) {
-        articles.push({ title: 'No news available', description: '', url: '', urlToImage: '' });
-        } else {
-          setArticles(data.articles.slice(0, 3));
-        }
+
+        setArticles(data.response.results.slice(0, 10));
       } catch (e: any) {
         setError(e.message);
       } finally {
@@ -43,28 +51,23 @@ const NewsFeed: React.FC<Props> = ({ coinId }) => {
     fetchArticles();
   }, [coinId]);
 
-  if (loading) return(
-    
-    <Card style={{ display: 'flex', flexDirection: 'row', gap: '20px', padding: '20px' }}>
-        Loading...
-    </Card>);
+  if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
 
   return (
-    <Card style={{ display: 'flex', flexDirection: 'row', gap: '20px', padding: '20px' }}>
+    <NewsGrid>
       {articles.map((article, index) => (
-        <Card key={index} style={{ display: 'flex', flexDirection: 'column', marginBottom: '20px', gap: "20px", padding: '20px',flex: 0.33 }}>
-          <img src={article.urlToImage} alt="Article" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
-          <CardHeader>{article.title}</CardHeader>
-          <CardDescription>{article.description}</CardDescription>
+        <Card key={index}>
+          <CardHeader>{article.webTitle}</CardHeader>
           <CardContent>
-            <a href={article.url} target="_blank" rel="noreferrer" style={{ textDecoration: 'underline', color: 'blue' }}>
-              Read more
+            <CardDescription>Published: {new Date(article.webPublicationDate).toLocaleDateString()}</CardDescription>
+            <a href={article.webUrl} target="_blank" rel="noopener noreferrer" style={{ color:'blue' }}>
+              Read more...
             </a>
           </CardContent>
         </Card>
       ))}
-    </Card>
+    </NewsGrid>
   );
 };
 
